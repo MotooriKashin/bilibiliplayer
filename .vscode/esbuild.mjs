e00277d89c0695bed32079d368d00d2372d37c7f
MotooriKashin <60416767+MotooriKashin@users.noreply.github.com>
2023-02-18 20:44:56 +0800

    1

--- .vscode/esbuild.mjs
+++ .vscode/esbuild.mjs
@@ -7,14 +7,13 @@ esbuild.build({
     ],
     // target: "chrome76", // 目标标准
     bundle: true, // 是否打包
-    // sourcemap: true, // map文件
+    sourcemap: true, // map文件
     minify: true, // 是否压缩
     outdir: 'dist', // 输出目录
     outbase: "src", // 输入目录
     format: 'iife', // 输出格式
     treeShaking: true, // 清除无效代码
-    // metafile: true, // 打印报表
-    // charset: 'utf8', // 文件编码
+    charset: 'utf8', // 文件编码
     loader: { // 文件对应的解析方式
         '.html': 'text',
         '.svg': 'text',
